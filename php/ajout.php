840585aed62025d7873e689c1e40351c89fb2c35
nups92722 <nicolas.leprince20@orange.fr>
2024-05-27 14:27:13 +0200

    add : page to add car

---
+++ php/ajout.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/basic.css">
+    <link rel="stylesheet" href="../css/onglet.css">
+    <script src="../js/onglet.js"></script>
+    <script src="../js/ajout.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        try {
+            $connexion = new PDO('mysql:host=localhost;dbname=locauto', 'root', ''); // ajouter le nom de la base de donne
+            } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage() . "<br/>";
+            die();
+        }
+    ?>
+
+    <h1>ajouter une voiture</h1>
+
+    <form action="validation.php"> <!-- bloc pour ajouter une voiture -->
+        <div> <!-- bloc modele de l'ajout de voiture -->
+            <h2>modele</h2>
+
+            <div id="modele">
+                <input type="checkbox" name="new_modele" id="new_modele" class="invisible"><!-- checkbox pour verifier si c'est un nouveau modele -->
+
+                <ul class="tab_menu"> <!-- bloc de selection pour le cas modele existant/nouveau -->
+                    <li class="tab selected">selectionner un modele</li>
+                    <li class="tab">nouveau modele</li>
+                </ul>
+
+                <div class="content"> <!-- bloc de selection de modele existant -->
+                    <p>selectionner le modele</p>
+
+                    <?php // creation du bloc selection des modeles
+                        try {
+                            $requete = 'SELECT * FROM modele ORDER BY modele';
+                            $resultat = $connexion->query($requete);
+                            $liste_modele = '<select id="e" id="ef">';
+
+                            while ($ligne = $resultat->fetch()) {
+                                $liste_modele .= '<option value="'.$ligne['id_modele'].'">'.$ligne['modele'].'</option>';
+                            }
+                            $liste_modele .= '</select>';
+                            echo ($liste_modele);
+                        } catch (PDOException $e) {
+                            echo "Erreur : " . $e->getMessage() . "<br/>";
+                            die();
+                        }
+                    ?>
+                </div>
+
+                <div class="content invisible"> <!-- bloc d'ajout dun nouveau modele -->
+                    <p>nom modele :</p>
+                    <input name="modele" type="text">
+                    
+                    <p>nombre de place :</p>
+                    <input name="number" type="number">
+
+                    <div id="categorie"> <!-- bloc de selection pour le cas categorie existant/nouveau -->
+                        <input type="checkbox" name="new_categorie" id="new_categorie" class="invisible"><!-- checkbox pour verifier si c'est une nouvelle categorie -->
+
+                        <ul  class="tab_menu">
+                            <li class="tab selected">categorie</li>
+                            <li class="tab">nouvelle categorie</li>
+                        </ul>
+
+                        <div class="content"> <!-- bloc de selection de categorie existante -->
+                            <p>selectionner le modele</p>
+
+                            <?php // creation du bloc selection des categories
+                                try {
+                                    $requete = 'SELECT * FROM categorie ORDER BY categorie';
+                                    $resultat = $connexion->query($requete);
+                                    $liste_modele = '<select id="d" id="ef">';
+
+                                    while ($ligne = $resultat->fetch()) {
+                                        $liste_modele .= '<option value="'.$ligne['id_categorie'].'">'.$ligne['categorie'].'</option>';
+                                    }
+                                    $liste_modele .= '</select>';
+                                    echo ($liste_modele);
+                                } catch (PDOException $e) {
+                                    echo "Erreur : " . $e->getMessage() . "<br/>";
+                                    die();
+                                }
+                            ?>
+                        </div>
+                        
+                        <div class="content invisible"> <!-- bloc d'ajout dune nouvelle categorie -->
+                            <p>nom categorie :</p>
+                            <input name="categorie" type="text">
+                        </div> 
+                    </div>
+                </div>
+            </div>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>   
+            <div id="motorisation"> <!-- bloc de selection pour le cas type existant/nouveau -->
+                <input type="checkbox" name="new_motorisation" id="new_motorisation" class="invisible"><!-- checkbox pour verifier si c'est une nouvelle motorisation -->
+
+                <p>motorisation :</p>
+
+                <ul class="tab_menu">
+                    <li class="tab selected">type</li>
+                    <li class="tab">nouveau type</li>
+                </ul>
+
+                <div class="content"> <!-- bloc de selection de type existant -->
+                    <p>selectionner le type</p>
+
+                    <?php // creation du bloc selection du types
+                    try {
+                        $requete = 'SELECT * FROM type_motorisation ORDER BY motorisation';
+                        $resultat = $connexion->query($requete);
+                        $liste_modele = '<select id="f" id="ef">';
+
+                        while ($ligne = $resultat->fetch()) {
+                            $liste_modele .= '<option value="'.$ligne['id_type_motorisation'].'">'.$ligne['motorisation'].'</option>';
+                        }
+                        $liste_modele .= '</select>';
+                        echo ($liste_modele);
+                    } catch (PDOException $e) {
+                        echo "Erreur : " . $e->getMessage() . "<br/>";
+                        die();
+                    }
+                    ?>
+                </div>
+                
+                <div class="content invisible"> <!-- bloc d'ajout dun nouveau type -->
+                    <p>nom type :</p>
+                    <input name="type" type="text">
+                </div>
+            </div>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+                                                                                                    <br>
+            <div>
+                <p>immatriculation :</p>
+                <input name="immatriculation" type="text" minlength="13" maxlength="13">
+            </div>
+
+            <div>
+                <p>compteur :</p>
+                <input name="compteur" type="number">
+            </div>
+        </div>
+
+        <input name="submit" type="submit" value="valider">
+    </form>
+</body>
+</html>
