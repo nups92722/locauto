340cc7c304619a9c7c0a7c64f12cac954a9803b3
nups92722 <nicolas.leprince20@orange.fr>
2024-05-27 15:45:41 +0200

    add : function for requirement gestion

--- js/ajout.js
+++ js/ajout.js
@@ -1,13 +1,28 @@
+function gestion_requirement(content, value) {
+    let input = content.querySelector('input');
+
+    if (input != null) {
+        for (let i = 0; i < input.length; i++) {
+            input[i].required = value;
+        }
+    }
+}
+
 function checked_box(id, id_checkbox) {
     let div_onglet = document.getElementById(id);
     let tabs = collect_tab(div_onglet);
+    let contents = collect_content(div_onglet);
     let checkbox = document.getElementById(id_checkbox);
-
+    
     tabs[0].addEventListener("click", () => {
         checkbox.checked = false;
+        gestion_requirement(contents[0], true);
+        gestion_requirement(contents[1], false);
     });
     tabs[1].addEventListener("click", () => {
         checkbox.checked = true;
+        gestion_requirement(contents[0], false);
+        gestion_requirement(contents[1], true);
     });
 }
 
