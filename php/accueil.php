44eea0ca255280d9f973cfd0e508a08ba6099f83
nups92722 <nicolas.leprince20@orange.fr>
2024-05-27 13:16:00 +0200

    add : execution of request after validation

--- php/accueil.php
+++ php/accueil.php
@@ -6,6 +6,29 @@
     <title>Locauto</title>
 </head>
 <body>
+    <?php
+        session_start();
+
+        try {
+            $connexion = new PDO('mysql:host=localhost;dbname=locauto', 'root', ''); // ajouter le nom de la base de donne
+            } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage() . "<br/>";
+            die();
+        }
+    ?>
+
+    <?php
+        try {
+            if (isset($_POST['valider'])) {
+                $resultat = $connexion->exec($_SESSION['request']);
+                echo ('<script>alert("l\'action à bien été prise en compte");</script>');
+            }
+        } catch (PDOException $e) {
+            echo ("Erreur : " . $e->getMessage() . "<br/>");
+            die();
+        }
+    ?>
+
     <h1>locauto</h1>
 
     <ul>
