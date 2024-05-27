e3b0c910435d1c812ee6a84eac66dbacffcaa36d
nups92722 <nicolas.leprince20@orange.fr>
2024-05-27 13:18:02 +0200

    add : creation of request for remove car and redirection to validation page

--- php/supprime.php
+++ php/supprime.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <?php
+        session_start();
         try {
             $connexion = new PDO('mysql:host=localhost;dbname=locauto', 'root', ''); // ajouter le nom de la base de donne
             } catch (PDOException $e) {
@@ -17,13 +18,13 @@
 
     <h1>supprimer une voiture</h1>
 
-    <form action="validation.php"> <!-- bloc pour supprimer une voiture -->
+    <form action="validation.php" method="post"> <!-- bloc pour supprimer une voiture -->
         <div>
             <p>immatriculation :</p>
-            <input name="immatriculation" type="text" minlength="13" maxlength="13" required>
+            <input name="immatriculation" type="text" minlength="11" maxlength="11" required>
         </div>
 
-        <input name="supprimer" type="submit" value="valider">
+        <input name="supprimer" type="submit" value="supprimer">
     </form>
 </body>
 </html>
