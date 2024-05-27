d591c711d3049657881a9430a58214a8fa22aa54
nups92722 <nicolas.leprince20@orange.fr>
2024-05-27 14:30:16 +0200

    add : checkbox to know if the category already exists

--- js/ajout.js
+++ js/ajout.js
@@ -1,6 +1,21 @@
+function checked_box(id, id_checkbox) {
+    let div_onglet = document.getElementById(id);
+    let tabs = collect_tab(div_onglet);
+    let checkbox = document.getElementById(id_checkbox);
+
+    tabs[0].addEventListener("click", () => {
+        checkbox.checked = false;
+    });
+    tabs[1].addEventListener("click", () => {
+        checkbox.checked = true;
+    });
+}
+
 window.onload = function() {
-    console.log("debut ficher js ajout");
     create_onglet("modele");
+    checked_box("modele", "new_modele");
     create_onglet("categorie");
-    create_onglet("type");
+    checked_box("categorie", "new_categorie");
+    create_onglet("motorisation");
+    checked_box("motorisation", "new_motorisation");
 };
