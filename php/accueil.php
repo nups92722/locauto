697a9357402ff14bf867f94084b82619a7756e77
nups92722 <nicolas.leprince20@orange.fr>
2024-05-27 09:49:30 +0200

    add : home page

---
+++ php/accueil.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Locauto</title>
+</head>
+<body>
+    <h1>locauto</h1>
+
+    <ul>
+        <li><a href="ajout.php">ajouter</a></li>
+        <li><a href="supprime.php">supprimer</a></li>
+    </ul>
+</body>
+</html>
